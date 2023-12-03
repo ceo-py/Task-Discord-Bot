a38a9ec7a780fa48a90c1cd846caeb0bd4007986
ceo-py <sfmf@abv.bg>
2023-12-03 14:36:26 +0200

    migrate new pictures links

--- i_task.py
+++ i_task.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from buttons.searching_task_buttons import LanguageSearchingButtons
 from buttons.language_buttons import LanguageButtons, discord, os
 from prepare_answer.generate_message import show_result_message
+from show_task.show_task import ShowTask
 from stats.add_stats_json import load_stats
 from select_menus.select import Select, exams
 
@@ -19,6 +20,7 @@ class PersistentViewBot(commands.Bot):
 
 
 client = PersistentViewBot()
+ST = ShowTask()
 
 
 @client.event
@@ -42,7 +44,6 @@ async def add(ctx):
 
 @client.command(aliases=["py", "cs", "java", "js", "html", "mssql"])
 async def task(ctx, *task):
-
     if ctx.invoked_with == "task":
         await ctx.author.send(embed=embed_for_itask(), view=LanguageSearchingButtons())
         return
@@ -60,31 +61,32 @@ async def help(ctx):
         colour=discord.Colour.gold(),
     )
     embed.set_thumbnail(
-        url="https://cdn.discordapp.com/attachments/983670671647313930/994162444467445870"
-        "/pnghut_customer-service-technical-support.png"
+        # url="https://cdn.discordapp.com/attachments/983670671647313930/994162444467445870"
+        #     "/pnghut_customer-service-technical-support.png"
+        url="https://github.com/ceo-py/Project-Pictures/blob/main/Itask/chat_icon.png?raw=true"
     )
     embed.add_field(
         name=f"?py task name - for Python\n?cs task name - for C#\n"
-        f"?java task name - for Java\n"
-        f"?js task name - for JavaScript\n"
-        f"?html task name - for html and css\n"
-        f"?mssql task name - for MS-MSQL",
+             f"?java task name - for Java\n"
+             f"?js task name - for JavaScript\n"
+             f"?html task name - for html and css\n"
+             f"?mssql task name - for MS-MSQL",
         value=f"```fix\nWhen you ask me for a task by its full name "
-        f"(or at least part of its name) I will search for it and show you the results. "
-        f"If you are not sure how to ask me - see the example below.```[Command example]("
-        f"https://cdn.discordapp.com/attachments/983670671647313930/1056160608917131294/image.png)",
+              f"(or at least part of its name) I will search for it and show you the results. "
+              f"If you are not sure how to ask me.```[Read More]("
+              f"https://www.ceo-py.eu/DiscordBot/)",
         inline=False,
     )
     embed.add_field(
         name="/itask",
         value="```fix\nThis command will bring menu with all languages that currently are supported for "
-        "task"
-        "searching.```",
+              "task"
+              "searching.```",
     )
     embed.add_field(
         name=f"?help",
         value=f"```fix\nHave no fear - I task is here! Oops - I have said that already, ain't I? "
-        f"Well...  if you see this, then you are looking at my ?help, so enjoy it!```",
+              f"Well...  if you see this, then you are looking at my ?help, so enjoy it!```",
         inline=False,
     )
     await ctx.author.send(embed=embed)
@@ -95,34 +97,35 @@ async def rounding(interaction: discord.Interaction):
     embed = discord.Embed(
         title="**Закръгление!!!**",
         description="При използване на **f2/2f/2** цифрата 2 представлява колко знака след "
-        "десетичната точка желаем да покажем! За всички езици когато желаем да е "
-        "до най-близкото цяло число **надолу (3.87 -> 3)** може да използваме директно INT. "
-        "При желание да покажете абсолютна стойност ползвайте **ABS (Math.abs / abs())**\n\n"
-        "**Python**```py\n"
-        "import math\n"
-        "x = 3.14159\n"
-        "print(f'{x:.2f}') # 3.14\n"
-        "print(math.floor(x)) # 3\n"
-        "print(math.ceil(x)) # 4```\n"
-        "**C#**```cs\n"
-        "double x = 3.14159;\n"
-        'Console.WriteLine($"{x:f2}"); // 3.14\n'
-        "Console.WriteLine(Math.Floor(x)); // 3\n"
-        "Console.WriteLine(Math.Ceiling(x)); // 4```\n"
-        "**JAVA**```java\n"
-        "double x = 3.14159;\n"
-        'System.out.printf("Total: %.2f", x); // 3.14\n'
-        "System.out.println(Math.floor(x)); // 3.0\n"
-        "System.out.println(Math.ceil(x)); // 4.0```\n"
-        "**JS**```js\n"
-        "let x = 3.14159;\n"
-        "console.log(x.toFixed(2)); // 3.14\n"
-        "console.log(Math.floor(x)); // 3\n"
-        "console.log(Math.ceil(x)); // 4```\n",
+                    "десетичната точка желаем да покажем! За всички езици когато желаем да е "
+                    "до най-близкото цяло число **надолу (3.87 -> 3)** може да използваме директно INT. "
+                    "При желание да покажете абсолютна стойност ползвайте **ABS (Math.abs / abs())**\n\n"
+                    "**Python**```py\n"
+                    "import math\n"
+                    "x = 3.14159\n"
+                    "print(f'{x:.2f}') # 3.14\n"
+                    "print(math.floor(x)) # 3\n"
+                    "print(math.ceil(x)) # 4```\n"
+                    "**C#**```cs\n"
+                    "double x = 3.14159;\n"
+                    'Console.WriteLine($"{x:f2}"); // 3.14\n'
+                    "Console.WriteLine(Math.Floor(x)); // 3\n"
+                    "Console.WriteLine(Math.Ceiling(x)); // 4```\n"
+                    "**JAVA**```java\n"
+                    "double x = 3.14159;\n"
+                    'System.out.printf("Total: %.2f", x); // 3.14\n'
+                    "System.out.println(Math.floor(x)); // 3.0\n"
+                    "System.out.println(Math.ceil(x)); // 4.0```\n"
+                    "**JS**```js\n"
+                    "let x = 3.14159;\n"
+                    "console.log(x.toFixed(2)); // 3.14\n"
+                    "console.log(Math.floor(x)); // 3\n"
+                    "console.log(Math.ceil(x)); // 4```\n",
         colour=discord.Colour.gold(),
     )
     embed.set_thumbnail(
-        url="https://cdn.discordapp.com/attachments/983670671647313930/1068903570335879318/rounding_23.png"
+        # url="https://cdn.discordapp.com/attachments/983670671647313930/1068903570335879318/rounding_23.png"
+        url="https://github.com/ceo-py/Project-Pictures/blob/main/Itask/rounding_23.png?raw=true"
     )
     await interaction.response.send_message(embed=embed)
 
@@ -134,49 +137,51 @@ async def percent(interaction: discord.Interaction):
     embed = discord.Embed(
         title="Как да изчисляваме проценти!",
         description="**Подробна информация** [ТУК](https://www.kaksepravibg.com/kak-se-izchisliava-procent/?fbclid"
-        "=IwAR1vuZo1zZnaW0eAZnlhTT3ClPxMtojowO1Y9ORzrqc1OfH4-3XSTe-PjcY)",
+                    "=IwAR1vuZo1zZnaW0eAZnlhTT3ClPxMtojowO1Y9ORzrqc1OfH4-3XSTe-PjcY)",
         colour=discord.Colour.gold(),
     )
     embed.set_thumbnail(
-        url="https://cdn.discordapp.com/attachments/983670671647313930/1061681874239500358/pngegg_1.png"
+        #     url="https://cdn.discordapp.com/attachments/983670671647313930/1061681874239500358/pngegg_1.png"
+        # )
+        url="https://github.com/ceo-py/Project-Pictures/blob/main/Itask/percent.png?raw=true"
     )
     embed.add_field(
         name="**Пример: 1**",
         value="Имаме 10 ябълки. 10 ябълки са 100%. Изяждаме 2 ябълки. От тук следва, че сме изяли **__2 / 10 х 100% = "
-        "20%__**"
-        "от ябълките и оставаме с 80% от първоначалния брой ябълки."
-        " От този пример можем да изведем формулата за смятане на процент от число"
-        " процент от цяло = **__част х 100 / цялото__**"
-        " или в нашия пример:"
-        " **__20%  = 2 ябълки х 100  / 10 ябълки__**",
+              "20%__**"
+              "от ябълките и оставаме с 80% от първоначалния брой ябълки."
+              " От този пример можем да изведем формулата за смятане на процент от число"
+              " процент от цяло = **__част х 100 / цялото__**"
+              " или в нашия пример:"
+              " **__20%  = 2 ябълки х 100  / 10 ябълки__**",
         inline=False,
     )
     embed.add_field(
         name="**Пример: 2**",
         value="Имаме промоция 8% на стока на цена 200 лв. Използваме следната формула:"
-        " **__200 х 8 / 100 = 16 лв__**. Следователно цената на стоката е 184 лв.",
+              " **__200 х 8 / 100 = 16 лв__**. Следователно цената на стоката е 184 лв.",
         inline=False,
     )
     embed.add_field(
         name="**Пример: 3**",
         value="**98 * 0.9 = 88.2** - Намаляме числото 98 с 10%.\n"
-        "**98 * 0.73 = 71.54** - Намаляме числото 98 с 27%.\n"
-        "Видно от примерa за намалениe числото започва с 0. "
-        "след това процента с които желаете да намалите числото ако е примерно 15%. "
-        "100 - 15 = 85 или 0.85 * желаното число.\n"
-        "**98 * 1.10 = 107.8** - Увеличаваме числото 98 с 10%.\n"
-        "**98 * 1.35 = 132.3** - Увеличаваме числото 98 с 35%.\n"
-        "За увеличение число започва с 1. "
-        "след това процента с които желаем да направим увеличението. "
-        "Примерно 47% ще бъде 1.47 * желаното число.",
+              "**98 * 0.73 = 71.54** - Намаляме числото 98 с 27%.\n"
+              "Видно от примерa за намалениe числото започва с 0. "
+              "след това процента с които желаете да намалите числото ако е примерно 15%. "
+              "100 - 15 = 85 или 0.85 * желаното число.\n"
+              "**98 * 1.10 = 107.8** - Увеличаваме числото 98 с 10%.\n"
+              "**98 * 1.35 = 132.3** - Увеличаваме числото 98 с 35%.\n"
+              "За увеличение число започва с 1. "
+              "след това процента с които желаем да направим увеличението. "
+              "Примерно 47% ще бъде 1.47 * желаното число.",
         inline=False,
     )
     embed.add_field(
         name="**Намиране на число по даден процент**",
         value="Ако сме закупили стока, намалена с 20% и сме платили цена от 160 лв, колко е била цената на стоката "
-        "без намалението. Намалението е било 20%, следователно сме платили 80% от цената на стоката. Стоката е "
-        "струвала Х лева. Смятаме по формулата: **__80 / 100 * Х = 160__** Като пресметнем уравнението, "
-        "се получава, че цената преди намалението е била 200 лв.",
+              "без намалението. Намалението е било 20%, следователно сме платили 80% от цената на стоката. Стоката е "
+              "струвала Х лева. Смятаме по формулата: **__80 / 100 * Х = 160__** Като пресметнем уравнението, "
+              "се получава, че цената преди намалението е била 200 лв.",
         inline=False,
     )
     await interaction.response.send_message(embed=embed)
@@ -208,7 +213,9 @@ async def logsolution(interaction: discord.Interaction):
         colour=discord.Colour.gold(),
     )
     embed.set_thumbnail(
-        url="https://cdn.discordapp.com/attachments/983670671647313930/1113488543235453061/pngwing.com.png"
+        #     url="https://cdn.discordapp.com/attachments/983670671647313930/1113488543235453061/pngwing.com.png"
+        # )
+        url="https://github.com/ceo-py/Project-Pictures/blob/main/Itask/help_icon.png?raw=true"
     )
     embed.add_field(
         name="**Отворете с друг Браузър**",
@@ -251,8 +258,8 @@ def embed_for_itask():
     embed = discord.Embed(
         title="Показвам примерни решения на задачите от СофтУни!",
         description="Изберете езика за който желаете да намерите пример и напишете името на задача.\n"
-        "Ако желаете може да ми задавате въпроси на лично съобщение <@970393820497838180> с команда за съответния език "
-        "`?py`, `?cs`, `?java`, `?js`, `?html` или `?mssql` и името на задачата.",
+                    "Ако желаете може да ми задавате въпроси на лично съобщение <@970393820497838180> с команда за съответния език "
+                    "`?py`, `?cs`, `?java`, `?js`, `?html` или `?mssql` и името на задачата.",
         colour=discord.Colour.gold(),
     )
     return embed
@@ -262,7 +269,7 @@ def embed_for_itask_exam():
     embed = discord.Embed(
         title="Провеждат се изпити към момента!!!",
         description="С цел да се осигури нормалното протичане на изпитите, бота няма да дава отгорови до приключването им.\n"
-        "Благодаря за разбирането и успех на изпита!",
+                    "Благодаря за разбирането и успех на изпита!",
         colour=discord.Colour.red(),
     )
     return embed
@@ -270,11 +277,16 @@ def embed_for_itask_exam():
 
 @client.tree.command(name="exam")
 async def test(interaction: discord.Interaction):
-
     if str(interaction.user) in os.getenv("OWNER") or str(
-        interaction.user
+            interaction.user
     ) in os.getenv("PERMIT"):
         await interaction.response.send_message(view=Select())
+
+
+# @client.tree.command(name="showtask", description="Available Task Solution")
+# async def show(interaction: discord.Interaction, language: ST.languages(), module: ST.modules(), course: ST.course(), task: ST.task()):
+#
+#         await interaction.response.send_message(f'{language}, {module}, {course}, {task}')
 
 
 client.run(os.getenv("TOKEN"))
