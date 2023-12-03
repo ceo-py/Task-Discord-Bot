733b7b36e25bc88e0e1a4d07572b634e1a708f2d
ceo-py <sfmf@abv.bg>
2023-12-03 14:40:17 +0200

    migrate new pictures links

--- prepare_answer/generate_message.py
+++ prepare_answer/generate_message.py
@@ -4,29 +4,35 @@ from stats.add_stats_json import add_stats
 
 LANGUAGE_DATA = {
     "cs": {
-        "thumbnail url": "https://cdn.discordapp.com/attachments/983670671647313930/1019383803535446096/pngegg.png",
+        # "thumbnail url": "https://cdn.discordapp.com/attachments/983670671647313930/1019383803535446096/pngegg.png",
+        "thumbnail url": "https://github.com/ceo-py/Project-Pictures/blob/main/Itask/csharp_lang_icon.png?raw=true",
         "data": "cs",
     },
     "py": {
-        "thumbnail url": "https://cdn.discordapp.com/attachments/983670671647313930/994020525410111578/kisspng-python-"
+        # "thumbnail url": "https://cdn.discordapp.com/attachments/983670671647313930/994020525410111578/kisspng-python-"
+        "thumbnail url": "https://github.com/ceo-py/Project-Pictures/blob/main/Itask/python_lang_icon.png?raw=true"
         "general-purpose-programming-language-comput-python-programming-language-symphony-solution-"
         "5b6ee0c89ecd95.2067324515339931606505.png",
         "data": "python",
     },
     "java": {
-        "thumbnail url": "https://cdn.discordapp.com/attachments/983670671647313930/1052942041652404256/java.png",
+        # "thumbnail url": "https://cdn.discordapp.com/attachments/983670671647313930/1052942041652404256/java.png",
+        "thumbnail url": "https://github.com/ceo-py/Project-Pictures/blob/main/Itask/java_lang_icon.png?raw=true",
         "data": "java",
     },
     "js": {
-        "thumbnail url": "https://cdn.discordapp.com/attachments/983670671647313930/1056201559102476318/pngwing.com.png",
+        # "thumbnail url": "https://cdn.discordapp.com/attachments/983670671647313930/1056201559102476318/pngwing.com.png",
+        "thumbnail url": "https://github.com/ceo-py/Project-Pictures/blob/main/Itask/js_lang_icon.png?raw=true",
         "data": "js",
     },
     "html": {
-        "thumbnail url": "https://cdn.discordapp.com/attachments/983670671647313930/1061697692587266158/pngfind.com-javascript-logo-png-1506020.png",
+        # "thumbnail url": "https://cdn.discordapp.com/attachments/983670671647313930/1061697692587266158/pngfind.com-javascript-logo-png-1506020.png",
+        "thumbnail url": "https://github.com/ceo-py/Project-Pictures/blob/main/Itask/html_css_lang_icon.png?raw=true",
         "data": "html_css",
     },
     "mssql": {
-        "thumbnail url": "https://cdn.discordapp.com/attachments/983670671647313930/1070267493379227729/pngwing.com.png",
+        # "thumbnail url": "https://cdn.discordapp.com/attachments/983670671647313930/1070267493379227729/pngwing.com.png",
+        "thumbnail url": "https://github.com/ceo-py/Project-Pictures/blob/main/Itask/mssql_lang_icon.png?raw=true",
         "data": "mssql",
     },
 }
